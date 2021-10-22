10246a5c62a4d066df8b158031fd7b9f72d8214f
Baku <Baku@DESKTOP-I64A340>
2021-10-22 11:33:51 +0400

    Add project files.

---
+++ Calculyatr1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculyatr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
